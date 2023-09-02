5d71ab639d4c7c841871c0af7ba89cf0a2f5b210
Bhuiyan Shadman Wasif <waseefafridi39@gmail.com>
2023-09-02 04:17:07 -0700

    Added Dark-Theme

--- index.js
+++ index.js
@@ -3,128 +3,111 @@ const search = document.querySelector(".search__btn");
 const searchResults = document.querySelector(".search__results");
 const loading = document.querySelector(".loading");
 const filter = document.querySelector("#filter");
-const toggle = document.querySelector(".toggle")
-let searchValue = '';
+const toggle = document.querySelector(".toggle");
+let searchValue = "";
 let isToggle = false;
 
+// Renders movies based on the selected filter value.
 async function renderMovie(filterValue) {
-    showLoading();
-    
-    const movieData = await fetchMovieData();
-    if (!movieData) {
-        return; // Handle error or no data
-    }
+  showLoading();
 
-    const movieArray = extractMovieArray(movieData);
+  const movieData = await fetchMovieData();
+  if (!movieData) {
+    return;
+  }
 
-    sortMovies(movieArray, filterValue);
+  const movieArray = extractMovieArray(movieData);
 
-    displayMovies(movieArray);
+  sortMovies(movieArray, filterValue);
+
+  displayMovies(movieArray);
 }
-
+// Shows a loading indicator while fetching movie data.
 function showLoading() {
-    loading.classList.add("movie__loading");
-    movieList.style.visibility = 'hidden';
+  loading.classList.add("movie__loading");
+  movieList.style.visibility = "hidden";
 }
 
+// Fetches movie data from the OMDB API.
 async function fetchMovieData() {
-    try {
-        const movie = await fetch(`https://www.omdbapi.com/?apikey=e43bdcdf&s=${searchValue}`);
-        return await movie.json();
-    } catch (error) {
-        console.error("An error occurred while fetching or processing the data:", error);
-        // Can display error message or return null
-        return null;
-    }
+  try {
+    const movie = await fetch(
+      `https://www.omdbapi.com/?apikey=e43bdcdf&s=${searchValue}`
+    );
+    return await movie.json();
+  } catch (error) {
+    console.error(
+      "An error occurred while fetching or processing the data:",
+      error
+    );
+    return null;
+  }
 }
 
+// Extracts the movie array from the movie data.
 function extractMovieArray(movieData) {
-    return movieData.Search || [];
+  return movieData.Search || [];
 }
 
+// Sorts the movies based on the selected filter value.
 function sortMovies(movieArray, filterValue) {
-    if (filterValue === 'New To Old') {
-        movieArray.sort((a, b) => b.Year - a.Year);
-    } else if (filterValue === 'Old To New') {
-        movieArray.sort((a, b) => a.Year - b.Year);
-    }
+  if (filterValue === "New To Old") {
+    movieArray.sort((a, b) => b.Year - a.Year);
+  } else if (filterValue === "Old To New") {
+    movieArray.sort((a, b) => a.Year - b.Year);
+  }
 }
 
+// Displays the movies in the UI.
 function displayMovies(movieArray) {
-    setTimeout(() => {
-        loading.classList.remove("movie__loading");
-        movieList.innerHTML = movieArray.map((movie) => movieHTML(movie)).join("");
-        movieList.style.visibility = 'visible';
-    }, 1200);
+  setTimeout(() => {
+    loading.classList.remove("movie__loading");
+    movieList.innerHTML = movieArray.map((movie) => movieHTML(movie)).join("");
+    movieList.style.visibility = "visible";
+  }, 700);
 }
 
+// Event listener for the search input field.
+search.addEventListener("change", (e) => {
+  searchValue = e.target.value;
+  if (searchValue === "") {
+    searchResults.textContent = `Search Results For: `;
+  } else {
+    searchResults.textContent = `Search Results For: ${searchValue}`;
+    filter.style.display = "flex";
+    filter.value = "Sort";
+  }
+  renderMovie();
+});
 
-// async function renderMovie(filterValue){
-//     setTimeout(() => {
-//     loading.classList.add("movie__loading")
-//     }, 500);
-//     movieList.style.visibility = 'hidden'    
-//     const movie = await fetch(`https://www.omdbapi.com/?apikey=e43bdcdf&s=${searchValue}`);
-//     const movieData = await movie.json();
-//     let movieArray = movieData.Search;
-
-//     if (filterValue === 'New To Old'){
-//         movieArray.sort((a, b) => b.Year - a.Year)
-//     }
-
-//     else if (filterValue === 'Old To New'){
-//         movieArray.sort((a, b) => a.Year - b.Year)
-//     }
-
-//     setTimeout(() => {
-//         loading.classList.remove("movie__loading")
-//         movieList.innerHTML = movieArray.map((movie) => movieHTML(movie)).join("");
-//         movieList.style.visibility = 'visible'    
-//     }, 1200);
-
-// }
-
-search.addEventListener('change', (e)=> {
-    searchValue = e.target.value;
-    if (searchValue === ''){
-        searchResults.textContent = `Search Results For: `
-    }
-    else {
-        searchResults.textContent = `Search Results For: ${searchValue}`
-        filter.style.display = 'flex';
-        filter.value = 'Sort';
-    }
-    renderMovie();
-
-})
-
+// Event listener for the filter select input.
 filter.addEventListener("change", (e) => {
-    renderMovie(e.target.value)
-})
+  renderMovie(e.target.value);
+});
 
-function showMovie(imdb){
-    localStorage.setItem("imdb", imdb)
-    window.location.href = `${window.location.origin}/movie.html`
+// Redirects to the movie details page.
+function showMovie(imdb) {
+  localStorage.setItem("imdb", imdb);
+  window.location.href = `${window.location.origin}/movie.html`;
 }
 
-function movieHTML(movie){
-    return `
+// Generates HTML for a movie item.
+function movieHTML(movie) {
+  return `
         <div class="movie" onclick = "showMovie('${movie.imdbID}')">
             <img class="movie__poster" src=${movie.Poster} alt="">
             <h3 class="movie__title">${movie.Title}</h3>
             <h3 class="movie__year">${movie.Year}</h3>
         </div>
-    `
+    `;
 }
 
-toggle.addEventListener('click', () => {
-    isToggle = !isToggle
-    if (isToggle){
-        document.body.classList.add("toggleContrast")
-    }
-    else{
-        document.body.classList.remove("toggleContrast")
-    }
-
-})
-
+// Event listener for the toggle button to change the contrast.
+toggle.addEventListener("click", () => {
+  isToggle = !isToggle;
+  if (isToggle) {
+    document.body.classList.add("toggleContrast");
+  } else {
+    document.body.classList.remove("toggleContrast");
+  }
+});
--- movie.js
+++ movie.js
@@ -1,17 +1,21 @@
 //https://www.omdbapi.com/?i=tt7767422&apikey=e43bdcdf
-const imdb = localStorage.getItem("imdb")
-const content = document.querySelector(".movie__content")
+const imdb = localStorage.getItem("imdb");
+const content = document.querySelector(".movie__content");
+const toggle = document.querySelector(".toggle");
+let isToggle = false;
 
-async function main(imdb){
-    const movie = await fetch(`https://www.omdbapi.com/?i=${imdb}&apikey=e43bdcdf`)
-    const movieData = await movie.json()
-    content.innerHTML = movieHTML(movieData)
+async function main(imdb) {
+  const movie = await fetch(
+    `https://www.omdbapi.com/?i=${imdb}&apikey=e43bdcdf`
+  );
+  const movieData = await movie.json();
+  content.innerHTML = movieHTML(movieData);
 }
 
-main(imdb)
+main(imdb);
 
-function movieHTML(movieData){
-    return `
+function movieHTML(movieData) {
+  return `
     <div class="left__section">
         <img class="movie__poster new__poster" src="${movieData.Poster}" alt="">
         <h3 class="movie__title">${movieData.Title}</h3>
@@ -19,35 +23,61 @@ function movieHTML(movieData){
     </div>
     <div class="right__section">
         <ul class="summary__points">
-            <li><h2 class="summary"><span class = "red">Genre: </span> ${movieData.Genre}</h2></li>
-            <li><h2 class="summary"><span class = "red">Type: </span> ${movieData.Type}</h2></li>
-            <li><h2 class="summary"><span class = "red">Writer: </span> ${movieData.Writer}</h2></li>
-            <li><h2 class="summary"><span class = "red">Actors: </span> ${movieData.Actors}</h2></li>
-            <li><h2 class="summary plot" ><span class = "red">Plot: </span> ${movieData.Plot}</h2></li>
-            <li><h2 class="summary"><span class = "red">Language: </span> ${movieData.Language}</h2></li>
-            <li><h2 class="summary"><span class = "red">Country: </span> ${movieData.Country}</h2></li>
-            <li><h2 class="summary"><span class = "red">Awards: </span> ${movieData.Awards}</h2></li>
-            <li><h2 class="summary"><span class = "red">imdb Rating: </span> ${movieData.imdbRating}</h2></li>
+            <li><h2 class="summary"><span class = "red">Genre: </span> ${
+              movieData.Genre
+            }</h2></li>
+            <li><h2 class="summary"><span class = "red">Type: </span> ${
+              movieData.Type
+            }</h2></li>
+            <li><h2 class="summary"><span class = "red">Writer: </span> ${
+              movieData.Writer
+            }</h2></li>
+            <li><h2 class="summary"><span class = "red">Actors: </span> ${
+              movieData.Actors
+            }</h2></li>
+            <li><h2 class="summary plot" ><span class = "red">Plot: </span> ${
+              movieData.Plot
+            }</h2></li>
+            <li><h2 class="summary"><span class = "red">Language: </span> ${
+              movieData.Language
+            }</h2></li>
+            <li><h2 class="summary"><span class = "red">Country: </span> ${
+              movieData.Country
+            }</h2></li>
+            <li><h2 class="summary"><span class = "red">Awards: </span> ${
+              movieData.Awards
+            }</h2></li>
+            <li><h2 class="summary"><span class = "red">imdb Rating: </span> ${
+              movieData.imdbRating
+            }</h2></li>
         </ul>
         <div class="ratings">
             ${ratingHTML(movieData.imdbRating)}
         </div>
                
-    </div>`
-
+    </div>`;
 }
 
-function ratingHTML(rating){
-    let ratings = ''
-    if (rating.toString().endsWith('.0')) {
-        for (let i = 0; i < Math.floor(rating); i++) {
-            ratings += `<i class="fas fa-star"></i>`;
-        }
-    } else {
-        for (let i = 0; i < Math.floor(rating); i++) {
-            ratings += `<i class="fas fa-star"></i>`;
-        }
-        ratings += `<i class="fas fa-star-half-alt"></i>`;
+function ratingHTML(rating) {
+  let ratings = "";
+  if (rating.toString().endsWith(".0")) {
+    for (let i = 0; i < Math.floor(rating); i++) {
+      ratings += `<i class="fas fa-star"></i>`;
     }
-    return ratings
+  } else {
+    for (let i = 0; i < Math.floor(rating); i++) {
+      ratings += `<i class="fas fa-star"></i>`;
+    }
+    ratings += `<i class="fas fa-star-half-alt"></i>`;
+  }
+  return ratings;
 }
+
+toggle.addEventListener("click", () => {
+  isToggle = !isToggle;
+  if (isToggle) {
+    document.body.classList.add("toggleContrast");
+  } else {
+    document.body.classList.remove("toggleContrast");
+  }
+});
