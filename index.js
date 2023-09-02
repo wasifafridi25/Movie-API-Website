4abf78d7b90d82ab6ad02b69c65ff70bbbf74d94
Bhuiyan Shadman Wasif <waseefafridi39@gmail.com>
2023-09-02 00:21:10 -0700

    Added the Sorting Option

--- index.js
+++ index.js
@@ -1,30 +1,51 @@
 const movieList = document.querySelector(".movies");
 const search = document.querySelector(".search__btn");
 const searchResults = document.querySelector(".search__results");
+const loading = document.querySelector(".loading");
+const filter = document.querySelector("#filter");
+let searchValue = '';
 
-async function main(searchValue){
-    movieList.classList.add("movie__loading")
-
+async function renderMovie(filterValue){
+    setTimeout(() => {
+    loading.classList.add("movie__loading")
+    }, 500);
+    movieList.style.visibility = 'hidden'    
     const movie = await fetch(`https://www.omdbapi.com/?apikey=e43bdcdf&s=${searchValue}`);
     const movieData = await movie.json();
+    let movieArray = movieData.Search;
 
-    movieList.classList.remove("movie__loading")
+    if (filterValue === 'New To Old'){
+        movieArray.sort((a, b) => b.Year - a.Year)
+    }
 
-    movieList.innerHTML = movieData.Search.map((movie) => movieHTML(movie)).join("");
+    else if (filterValue === 'Old To New'){
+        movieArray.sort((a, b) => a.Year - b.Year)
+    }
 
+    setTimeout(() => {
+        loading.classList.remove("movie__loading")
+        movieList.innerHTML = movieArray.map((movie) => movieHTML(movie)).join("");
+        movieList.style.visibility = 'visible'    
+    }, 1200);
 
-    
 }
 
 search.addEventListener('change', (e)=> {
     searchValue = e.target.value;
-    main(searchValue);
     if (searchValue === ''){
         searchResults.textContent = `Search Results For: `
     }
     else {
         searchResults.textContent = `Search Results For: ${searchValue}`
+        filter.style.display = 'flex';
+        filter.value = 'Sort';
     }
+    renderMovie();
+
+})
+
+filter.addEventListener("change", (e) => {
+    renderMovie(e.target.value)
 })
 
 function showMovie(imdb){
